4a41ddb9b58e777cfbc9b19656e59684e51e4c68
ylzhu <zhuyuanlv@cdtech.cn>
2015-02-03 15:08:11 +0800

    在服务器中去除不必要的文件.

--- clean.bat
+++
@@ -1,6 +0,0 @@
-@del /f mips\*.d mips\*.o
-@del /f drv\mips\*.o
-@del /f *.a
-@echo "clean cdt1028drv/mips/* cdt1028drv/drv/mips/* ./*.a"
-
-
--- tunermake.bat
+++
@@ -1,9 +0,0 @@
-call clean
-if exist ..\mipsel-elf\libcdt1028drv.a del ..\mipsel-elf\libcdt1028drv.a
-make
-copy libcdt1028drv.a ..\MIPSEL-ELF\libcdt1028drv.a
-copy libcdt1028drv.a ..\atv\libcdt1028drv.a
-cd ..
-call clean
-call sej
-cd cdt1028drv
